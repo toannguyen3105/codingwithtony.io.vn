0e796a6089151a3eb193a1e8c7f71338e8f62b23
Toan Nguyen <toannh@nal.vn>
2025-12-17 14:09:18 +0700

    feat: Configure Prettier, ESLint, Husky, and lint-staged for automated code formatting and linting.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,36 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+
+import playwright from 'eslint-plugin-playwright';
+import prettier from 'eslint-config-prettier';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  playwright.configs['flat/recommended'],
+  prettier,
+  {
+    rules: {
+      'playwright/no-skipped-test': 'warn',
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+    // Custom ignores
+    'node_modules/',
+    'test-results/',
+    'playwright-report/',
+    'blob-report/',
+    'playwright/.cache/',
+    'dist/',
+    '.git/',
+    'pnpm-lock.yaml',
   ]),
 ]);
 
